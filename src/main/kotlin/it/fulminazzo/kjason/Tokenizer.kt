1bc5836cfa695f542bf19bb4451d36cb9cdfa63a
Fulminazzo <fulminazzo21@gmail.com>
2025-01-15 17:55:04 +0100

    Renamed function to fromString and added JvmStatic notation for tests compatibility

---
+++ src/main/kotlin/it/fulminazzo/kjason/Tokenizer.kt
@@ -0,0 +1,34 @@
+package it.fulminazzo.kjason
+
+class Tokenizer {
+}
+
+enum class TokenType(val regex: String) {
+    // '-'
+    MINUS("-"),
+
+    // '0'
+    ZERO("0"),
+
+    // '1'.'9'
+    ONENINE("1-9"),
+
+    // 'CR' | 'LF' | 'TAB' | 'SPACE'
+    SPACE("[\r\n\t ]"),
+
+    EOF("");
+
+    companion object {
+
+        @JvmStatic
+        fun fromString(read: String): TokenType {
+            for (tokenType in entries.filter { it != EOF })
+                if (read.matches(Regex(tokenType.regex)))
+                    return tokenType
+            return EOF
+        }
+
+    }
+
+}
+

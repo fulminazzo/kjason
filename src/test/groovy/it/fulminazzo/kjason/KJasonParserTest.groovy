342f4ca27ac84076bc55106a710756be7db31c98
Fulminazzo <fulminazzo21@gmail.com>
2025-01-15 19:13:56 +0100

    Added tests for parseInteger function

--- src/test/groovy/it/fulminazzo/kjason/KJasonParserTest.groovy
+++ src/test/groovy/it/fulminazzo/kjason/KJasonParserTest.groovy
@@ -4,6 +4,32 @@ import spock.lang.Specification
 
 class KJasonParserTest extends Specification {
 
+    def 'parseInteger of #value should return #expected'() {
+        given:
+        def tokenizer = new KJasonParser(value)
+
+        and:
+        tokenizer.nextToken$KJason()
+
+        when:
+        def actual = tokenizer.parseInteger$KJason()
+
+        then:
+        actual == expected
+
+        where:
+        value         || expected
+        '0'           || 0
+        '+0'          || 0
+        '-0'          || 0
+        '1'           || 1
+        '+1'          || 1
+        '-1'          || -1
+        '1234567890'  || 1234567890
+        '+1234567890' || 1234567890
+        '-1234567890' || -1234567890
+    }
+
     def 'nextTokenSpaceless should ignore all the spaces before a Token'() {
         given:
         def tokenizer = new KJasonParser('        0')
